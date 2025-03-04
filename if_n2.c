7e5c18ebe13ea342eea500518efbc1b4026dce4c
kill13948 <michaelavs01@gmail.com>
2025-03-04 21:09:55 +0000

    if_ else

---
+++ if_n2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int main(){
+
+    float temperatura, umidade;
+    int estoque; 
+
+    printf(" entre com a temperatura! \n");
+    scanf("%f", &temperatura);
+
+    printf(" entre com a umidade! \n");
+    scanf("%f", &umidade);
+
+    printf(" entre com o estoque! \n");
+    scanf("%d", &estoque);
+
+    if (temperatura > 30)
+    {printf(" temperatura esta alta!\n");
+       }else{
+        printf("temperatura normal!\n");
+    }
+    if (umidade > 26){
+        printf("umidade elevada!\n");
+    }else
+    {printf("umidade normal!\n");
+    }
+
+    if (estoque > 10)
+    {printf(" estoque normal!\n");
+    }else
+    {printf(" estoque baixo!");
+    }
+    
+        
+    }
+    
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
